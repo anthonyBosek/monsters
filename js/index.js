096875d771b4cc8cafb2dc86d4f9820851166814
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-20 07:07:29 -0700

    complete lab

--- js/index.js
+++ js/index.js
@@ -1,47 +1,132 @@
-// Global Variables
+//* Global Variables
 const MONSTER_URL = "http://localhost:3000/monsters";
-const createMonster = document.querySelector("#create-monster");
-const monsterContainer = document.querySelector("#monster-container");
-const backButton = document.querySelector("#back");
-const forwardButton = document.querySelector("#forward");
+let limit = 50;
 let pageNum = 1;
 
-const getData = (url) => {};
-const postData = (url) => {};
-const getOneMonster = () => {};
-const createMonsterCard = () => {};
-const getAllMonsters = () => {
-  // getData()
-  // createMonsterCard()
+//* Universal get func w/ fetch
+const getData = (url) => {
+  return fetch(url).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw resp.statusText;
+    }
+  });
 };
-const handleSubmit = () => {};
-const createMonsterForm = () => {};
 
-const pageBack = () => {};
-const pageForward = () => {};
+//* Universal post func w/ fetch
+const postData = (url, payload) => {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(payload),
+  }).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    } else {
+      throw resp.statusText;
+    }
+  });
+};
 
-// Initialization Function to Encapsulate JS Startup
+//* Helper func to create cards and append to page
+const createMonsterCards = (monsters) => {
+  const monsterContainer = document.querySelector("#monster-container");
+  monsterContainer.innerHTML = "";
+  monsters.forEach(({ id, age, name, description }) => {
+    const div = document.createElement("div");
+    div.id = `monster-card-${id}`;
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h4 = document.createElement("h4");
+    h4.textContent = `Age: ${age}`;
+    const p = document.createElement("p");
+    p.textContent = `Info: ${description}`;
+
+    div.append(h2, h4, p);
+    monsterContainer.appendChild(div);
+  });
+};
+
+//* Get one monster data
+const getOneMonster = () => {}; //TODO: add functionality to fetch one by id
+
+//* Get all monsters data, then invoke card creator callback
+const getAllMonsters = () => {
+  getData(`${MONSTER_URL}/?_limit=${limit}&_page=${pageNum}`)
+    .then((monsters) => createMonsterCards(monsters))
+    .catch((err) => console.log("Error: ", err));
+};
+
+//* Form submit callback, post data to db
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const { name, age, description } = e.target;
+  const payload = {
+    name: name.value,
+    age: parseFloat(age.value),
+    description: description.value,
+  };
+  postData(MONSTER_URL, payload)
+    .then((added) => console.log("New Monster: ", added))
+    .catch((err) => console.log("Error: ", err));
+};
+
+//* Dynamically create form element and inputs
+const createMonsterForm = () => {
+  const createMonster = document.querySelector("#create-monster");
+
+  const monsterForm = document.createElement("form");
+  monsterForm.id = "monster-form";
+
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "name";
+  nameInput.placeholder = "Enter Monster Name";
+
+  const ageInput = document.createElement("input");
+  ageInput.type = "text";
+  ageInput.name = "age";
+  ageInput.placeholder = "Enter Monster Age";
+
+  const descInput = document.createElement("input");
+  descInput.type = "text";
+  descInput.name = "description";
+  descInput.placeholder = "Enter Monster Description";
+
+  const submit = document.createElement("input");
+  submit.type = "submit";
+  submit.value = "Create Monster";
+
+  monsterForm.append(nameInput, ageInput, descInput, submit);
+  createMonster.appendChild(monsterForm);
+};
+
+//* Page +/- to navigate & display monsters
+const pageBack = () => {
+  if (pageNum > 1) {
+    --pageNum;
+    getAllMonsters();
+  }
+};
+const pageForward = () => {
+  ++pageNum;
+  getAllMonsters();
+};
+
+//* Initialization Function to Encapsulate JS Startup
 const initialize = () => {
   getAllMonsters();
   createMonsterForm();
-
+  const backButton = document.querySelector("#back");
+  const forwardButton = document.querySelector("#forward");
+  const monsterForm = document.querySelector("#monster-form");
   backButton.addEventListener("click", pageBack);
   forwardButton.addEventListener("click", pageForward);
+  monsterForm.addEventListener("submit", handleSubmit);
 };
-// Initialize on Pageload
+//* Initialize on Pageload
 document.addEventListener("DOMContentLoaded", initialize);
-
-//
-// MVPs:
-// on load, dynamically create form
-// on load, fetch(GET) all monsters
-// invoke card create func for first 50 monsters
-// dynamically add monster cards to page
-// add functionality to capture form data on submit
-// on submit fetch(POST) new monster to add to db
-//
-// events:
-// page load
-// form submit
-// page forward
-// page backward
